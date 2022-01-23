b3ffcfaecaf38579528d9eb21922adaec3fc97cc
Duarte2002 <95648571+Duarte2002@users.noreply.github.com>
2022-01-23 23:25:44 +0000

    Add files via upload

---
+++ Projeto 3/css/script.js
@@ -0,0 +1,11 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm-classList.toggle('active');
+}
+
+let cart = document.queryselector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+}
